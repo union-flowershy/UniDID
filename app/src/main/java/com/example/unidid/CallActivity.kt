d3c13b47ec2edf2a65f7f7bc53c8a861da009b93
Junkyu Park <flowershy@unionpos.co.kr>
2022-10-24 17:49:17 +0900

    [OFFICE] - 고객호출 로그인 페이지 2022-10-24 PM 17:49

--- app/src/main/java/com/example/unidid/CallActivity.kt
+++ app/src/main/java/com/example/unidid/CallActivity.kt
@@ -1,24 +1,25 @@
 package com.example.unidid
 
 //import android.R
+
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.io.BufferedReader
-import java.io.DataInputStream
-import java.io.DataOutputStream
-import java.io.InputStreamReader
+import androidx.constraintlayout.widget.ConstraintLayout
 import java.io.PrintWriter
 import java.net.Socket
-import java.util.Scanner
 
 
 class CallActivity: AppCompatActivity() {
@@ -26,7 +27,7 @@ class CallActivity: AppCompatActivity() {
     lateinit var callNum: EditText
     lateinit var sendBtn: Button
     lateinit var textView: TextView
-
+    lateinit var callMain: ConstraintLayout
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,22 +36,45 @@ class CallActivity: AppCompatActivity() {
 
         callNum = findViewById<View>(R.id.call_id) as EditText
         sendBtn = findViewById<View>(R.id.btn_call) as Button
-        textView = findViewById(R.id.textView) as TextView
+        textView = findViewById<View>(R.id.textView) as TextView
+        callMain = findViewById<View>(R.id.all_main) as ConstraintLayout
         val animTransRight: Animation = AnimationUtils.loadAnimation(this, R.anim.aniutils)
-        val mImgView = findViewById(R.id.textView) as TextView
+        val mImgView = findViewById<View>(R.id.textView) as TextView
 
         mImgView.startAnimation(animTransRight)
 
-        sendBtn.setOnClickListener { view ->
+        //텍스트 바깥 레이아웃 클릭시 키보드 사라짐
+        callMain.setOnTouchListener(OnTouchListener { v, event ->
+            hideKeyboard()
+            false
+        })
+
+        sendBtn.setOnClickListener {
             val thread = NetworkThread()
             thread.start()
-            
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setTitle("전송되었습니다.")
+                .setMessage(callNum.text.toString())
+                .setPositiveButton("확인", DialogInterface.OnClickListener{dialog, id ->
+
+                })
+            builder.show()
         }
 
         val intent = getIntent()
         val storeName = intent.getStringExtra("storeName")
         val text_name = findViewById<TextView>(R.id.textView)
         text_name.setText(storeName)
+    }
+
+    //텍스트 바깥 레이아웃 클릭시 키보드 사라짐
+    fun hideKeyboard() {
+        val inputManager =
+            this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(
+            this.currentFocus!!.windowToken,
+            InputMethodManager.HIDE_NOT_ALWAYS
+        )
     }
 
     inner class NetworkThread: Thread() {
@@ -60,8 +84,8 @@ class CallActivity: AppCompatActivity() {
                 val socket: Socket
 
                 //소켓 서버 접속
-                //socket = Socket("192.168.10.19", 55555)
-                socket = Socket("192.168.1.164", 55555)
+                socket = Socket("192.168.10.19", 55555) // 사무실 IP
+                //socket = Socket("192.168.1.164", 55555) // 집 IP
                 System.out.println("서버 접속 성공")
 
                 // 서버에 보낼 주문번호 전송
@@ -69,14 +93,8 @@ class CallActivity: AppCompatActivity() {
                 val writer: PrintWriter = PrintWriter(output, true )
                     writer.println(callNum.text.toString())
                     Log.e("전송에 성공했습니다 전송 번호는 = ", callNum.text.toString())
-//                val dos = DataOutputStream(output)
-//                val orderNum = callNum.text.toString()
-//                dos.writeUTF(orderNum)
-//                Log.e("전송에 성공했습니다 전송 번호는 = ", orderNum)
+                    callNum.setText(null)
 
-                // 서버에서 보낸 메세지 읽는 Thread
-//                val input = socket.getInputStream()
-//                val dis = DataInputStream(input)
 
                 if(output != null) {
                     Log.e("전송에", "성공했습니다")
--- app/src/main/java/com/example/unidid/MainActivity.kt
+++ app/src/main/java/com/example/unidid/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.unidid
 
+import android.content.Context
 import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.internal.ViewUtils.hideKeyboard
 import java.io.*
 import java.net.*
 import java.lang.*
@@ -18,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var userId: EditText
     lateinit var userPwd: EditText
     lateinit var loginBtn: Button
+    lateinit var activityMain: ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +31,27 @@ class MainActivity : AppCompatActivity() {
         userId = findViewById<View>(R.id.edit_id) as EditText
         userPwd = findViewById<View>(R.id.edit_pwd) as EditText
         loginBtn = findViewById<View>(R.id.btn_login) as Button
+        activityMain = findViewById<View>(R.id.activityMain) as ConstraintLayout
         loginBtn.setOnClickListener(btnListener)
 
         Log.e("onCreate 실행", "정상 실행")
 
+
+        //텍스트 바깥 레이아웃 클릭시 키보드 사라짐
+        activityMain.setOnTouchListener(View.OnTouchListener { v, event ->
+            hideKeyboard()
+            false
+        })
+    }
+
+    //텍스트 바깥 레이아웃 클릭시 키보드 사라짐
+    fun hideKeyboard() {
+        val inputManager =
+            this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(
+            this.currentFocus!!.windowToken,
+            InputMethodManager.HIDE_NOT_ALWAYS
+        )
     }
 
     class CustomTask : AsyncTask<String?, Void?, String?>() {
@@ -40,8 +62,8 @@ class MainActivity : AppCompatActivity() {
         override fun doInBackground(vararg strings: String?): String {
             try {
                 var str: String?
-                //val url = URL("http://192.168.10.19:8080/exex/data.jsp")
-                val url = URL("http://192.168.1.164:8080/exex/data.jsp")
+                val url = URL("http://192.168.10.19:8080/exex/data.jsp")  // 사무실 IP
+                //val url = URL("http://192.168.1.164:8080/exex/data.jsp")  // 집 IP
                 val conn = url.openConnection() as HttpURLConnection
                 conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
                 conn.requestMethod = "POST"
